7205f56232af746a83bcacbc35d0ecdb41066793
prashant <sg8645375@gmail.com>
2023-08-06 17:00:30 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'tasks/level-1/task-1/index.html'),
+      }
+    }
+  }
+})
