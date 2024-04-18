d9beea7917beb258a919a05a9b689761418cec03
Prashant Gupta <86506121+shyamgdev@users.noreply.github.com>
2024-04-18 19:41:28 +0530

    adding projects dynamically

---
+++ tasks/level-1/task-2/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+{
+    title:"Netflix Clone",
+    img:"./assets/netflix-clone.png",
+    githubRepo:"https://github.com/shyamgdev/Codsoft/tree/main/tasks/level-1/task-1",
+    liveUrl:"https://codsoft.netlify.app/tasks/level-1/task-1"
+}
+]
+
+module.exports = projects
