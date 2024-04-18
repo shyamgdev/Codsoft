a68e67529d83f3364c9e8e09b8315bfb5ff738cd
prashant <sg8645375@gmail.com>
2024-04-18 23:03:40 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'tasks/level-1/task-1/index.html'),
+        main: resolve(__dirname, '/index.html'),
+        task1: resolve(__dirname, 'tasks/level-1/task-1/index.html'),
         task2: resolve(__dirname, 'tasks/level-1/task-2/index.html'),
         task3: resolve(__dirname, 'tasks/level-1/task-3/index.html'),
       }
     }
   }
-})
+});
